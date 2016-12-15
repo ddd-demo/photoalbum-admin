c52bc48c73ddb6d3b0398ee038eb5325469841a1
fengtaiguang1 <fengtaiguang1@163.com>
2016-12-15 18:58:04 +0800

    cc

--- src/main/java/com/fengtaiguang/photoalbum/admin/web/spring/controller/AlbumControllerSpring.java
+++ src/main/java/com/fengtaiguang/photoalbum/admin/web/spring/controller/AlbumControllerSpring.java
@@ -1,5 +1,17 @@
-package com.fengtaiguang.photoalbum.admin.web.controller.spring;
+package com.fengtaiguang.photoalbum.admin.web.spring.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fengtaiguang.photoalbum.application.dto.AlbumDto;
+
+@RestController
+@RequestMapping("/admin/album/")
 public class AlbumControllerSpring {
 
+	@RequestMapping("save")
+	public Boolean save(AlbumDto albutDto){
+		System.out.println(".............save:albutDto="+albutDto);
+		return true;
+	}
 }
